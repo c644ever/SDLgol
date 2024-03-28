358e4b9c1469406c9ebd7df1fcce53a682a0ceac
c644ever <c6443v3r@gmail.com>
2024-03-28 10:17:10 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include <time.h>
 
 #define QUITKEY 	SDLK_ESCAPE
-#define WIDTH 		800
-#define HIGHT 		800
+#define WIDTH 		400
+#define HIGHT 		400
 #define BLOCKSIZE 	4
 #define FELDH 		HIGHT/BLOCKSIZE
 #define FELDW 		WIDTH/BLOCKSIZE
@@ -18,8 +18,8 @@
 int quit=0;
 
 //    neigbors:        0      1     2     3     4     5     6     7     8
-bool ruledeath[9] = { true  ,true ,false,false,true ,true ,true ,true ,true  };
-bool rulebirth[9] = { false ,false,false,true ,false,false,false,false,false };
+bool ruledeath[9] = { true  ,true ,false,false,true ,true ,true ,true ,true  } ;
+bool rulebirth[9] = { false ,false,false,true ,false,false,false,false,false } ;
 
 	
 //SDL Declaration
@@ -44,7 +44,7 @@ unsigned char getWelt (int x,int y)
 	return (welt[x][y]);
 }
 
-void putWelt (int x, int y, unsigned char value)
+void setWelt (int x, int y, unsigned char value)
 {
 	if (x<0) x=FELDW-1 ;
 	if (y<0) y=FELDH-1 ;
@@ -55,13 +55,13 @@ void putWelt (int x, int y, unsigned char value)
 }
 
 void initFeld () {
-	int i,j ;
+	int x,y ;
 	
-	for (i=0 ; i < FELDW ; i++ )
+	for (x=0 ; x < FELDW ; x++ )
 	{
-		for ( j=0 ; j < FELDH ; j++ )
+		for ( y=0 ; y < FELDH ; y++ )
 		{
-			putWelt(i,j, (rand() % 2)*255 ) ;
+			setWelt(x,y, (rand() % 2)*255 ) ;
 		}
 	}
 }
@@ -117,19 +117,19 @@ void ExitClean() {
 
 void drawFeld( void ) {
 
-	int i,j,r,g,b ;
+	int x,y,r,g,b ;
 	unsigned char s;
 	
-	for (i=0 ; i < FELDW ; i++ ) {
-		for ( j=0 ; j < FELDH ; j++ ) {
-			s=getWelt(i,j);
+	for (x=0 ; x < FELDW ; x++ ) {
+		for ( y=0 ; y < FELDH ; y++ ) {
+			s=getWelt(x,y);
 	
 			switch (s) {
 				case 0	: r=50 	; g=0   ; b=0   ; break ;
 				case 255: r=189	; g=255 ; b=255	; break ;
 			}
-			rect.y=( j * BLOCKSIZE ) ; 
-			rect.x=( i * BLOCKSIZE ) ; 
+			rect.y=( x * BLOCKSIZE ) ; 
+			rect.x=( y * BLOCKSIZE ) ; 
 			rect.w= BLOCKSIZE-1 ;
 			rect.h= BLOCKSIZE-1 ;
 			SDL_SetRenderDrawColor(renderer,r,g,b,255);
@@ -142,7 +142,8 @@ void drawFeld( void ) {
 int main( void ){
 	int quit=0;
 	int ticks=0 ;
-	int x,y ;
+	int x,y ; 
+	int oldliving[10]={ 0 } , living=0; ;
 	
 	initFeld();
 
@@ -170,15 +171,21 @@ int main( void ){
 			}
 		}
 
-		int living=0;
-
 		for (x=0 ; x<FELDW ; x++) {
 			for (y=0 ; y<FELDH ; y++) {
-				putWelt(x,y,buffer[x][y]);
+				setWelt(x,y,buffer[x][y]);
 				if (buffer[x][y]==LIVE) living++ ;
 			}
 		}
-		if (ticks>10000) quit=1;
+
+		// Abbruchbedingung
+		/*
+		if (living==oldliving) quit=1 ;
+		printf ("%d %d\n",oldliving,living );
+		oldliving=living;
+		living=0;
+		*/
+		
 	}
 
 	ExitClean();
